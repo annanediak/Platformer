01e082a9f3324e319de561ccbdf750627dc33302
Anna Nediak <anna.nediak@outlook.com>
2023-01-05 12:52:55 -0500

    Added a new level

--- main.py
+++ main.py
@@ -66,6 +66,13 @@ while run:
     if abuntahead and vel < 0:
         vel = 0
 
+    if player_rect.right <= 0 or player_rect.left >= 500:
+        player_rect = Rect(levels[level].start_y,
+                           levels[level].start_y + 25,
+                           levels[level].start_x,
+                           levels[level].start_x + 25,
+                           (255, 0, 0))
+
     win.fill((0, 0, 0))
     levels[level].draw(win)
     player_rect.draw(win)
